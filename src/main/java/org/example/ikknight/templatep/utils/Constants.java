53373daaee2578d27e973bbc2d902a71cff294b3
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-06-30 22:53:42 -0500

    Basic changes from template

--- src/main/java/org/example/ikknight/templatep/utils/Constants.java
+++ src/main/java/org/example/ikknight/templatep/utils/Constants.java
@@ -1,8 +1,8 @@
 package org.example.ikknight.templatep.utils;
 
 public final class Constants {
-    public static final String DEFAULT_SUFFIX_VALUE = "[TEMPLATE-PLUGIN] ";
-    public static final String PLUGIN_NAME = "TEMPLATE-PLUGIN";
+    public static final String DEFAULT_SUFFIX_VALUE = "[ENGLISH-OR-SPANISH] ";
+    public static final String PLUGIN_NAME = "ENGLISH-OR-SPANISH";
 
     public static final class YAML{
         public static final String PREFIX_PATH = "prefix";
